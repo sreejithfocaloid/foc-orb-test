665a16cbe6ca8d594207885da482942193e7ffdb
sreejith <sreejiths@focaloid.com>
2022-09-14 12:22:54 +0530

    Docker Slim Xray

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #echo Hello "${PARAM_TO}"
 echo Installing Docker-Slim
-curl -sL https://raw.githubusercontent.com/docker-slim/docker-slim/master/scripts/install-dockerslim.sh | sudo -E bash -
-
+#curl -sL https://raw.githubusercontent.com/docker-slim/docker-slim/master/scripts/install-dockerslim.sh | sudo -E bash -
+docker pull dslim/docker-slim
 echo X-Ray Scan
 
 docker-slim xray --pull node --registry-account=sreejithfocaloid --registry-secret="${PARAM_KEY}"
