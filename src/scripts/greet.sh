55e1b2e9e7af1ef483ba30a2d1f8e54c26f80214
sreejith <sreejiths@focaloid.com>
2022-09-14 11:40:07 +0530

    Docker Slim Xray

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -2,3 +2,7 @@
 #echo Hello "${PARAM_TO}"
 echo Installing Docker-Slim
 curl -sL https://raw.githubusercontent.com/docker-slim/docker-slim/master/scripts/install-dockerslim.sh | sudo -E bash -
+
+echo X-Ray Scan
+
+docker-slim xray --pull node --target
