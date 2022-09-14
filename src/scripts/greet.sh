f3968e4353d184bb8afaa608618e865196f3284f
sreejith <sreejiths@focaloid.com>
2022-09-14 11:30:37 +0530

    Docker Slim Installation

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo Hello "${PARAM_TO}"
+#echo Hello "${PARAM_TO}"
+echo Installing Docker-Slim
+curl -sL https://raw.githubusercontent.com/docker-slim/docker-slim/master/scripts/install-dockerslim.sh | sudo -E bash -
