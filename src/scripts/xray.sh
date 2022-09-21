ecab442a6dd368f43e3a73d1df9dde38d363d00e
sreejith <sreejiths@focaloid.com>
2022-09-21 16:30:45 +0530

    Artifact testing

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #echo Hello "${PARAM_TO}"
 echo Installing Docker-Slim
-#curl -sL https://raw.githubusercontent.com/docker-slim/docker-slim/master/scripts/install-dockerslim.sh | sudo -E bash -
+curl -sL https://raw.githubusercontent.com/docker-slim/docker-slim/master/scripts/install-dockerslim.sh | sudo -E bash -
 # docker pull dslim/docker-slim
 echo X-Ray Scan : "${PARAM_IMAGE}"
 
-# docker-slim xray --pull --target "${PARAM_IMAGE}"
+docker-slim xray --pull --target "${PARAM_IMAGE}"
 
-# cat slim.report.json >> /tmp/artifact-xray;
+cat slim.report.json >> /tmp/artifact-xray;
 
 echo Syft sbom
 
