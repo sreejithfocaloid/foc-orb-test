003bb72fd2e918c2b8c93f557e4967f0d3e3090a
sreejith <sreejiths@focaloid.com>
2022-09-23 16:23:58 +0530

    Favourite test

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -15,7 +15,7 @@ fullName=$(cat slim.report.json  | jq -r '.source_image.identity.names[0]')
 echo "${shaId}"
 echo "${tag}"
 
-imageDetails=$(curl -X POST "https://platform.slim.dev/orgs/rko.24nRz6GvLBo9hah9dqmhHON820R/collections/rkcol.24pqmb2bNiDQiBU2zAQMUJjWiSh/images" -H  "accept: application/json" -H  "Authorization: Basic ${SAAS_KEY}" -H  "Content-Type: application/json" -d "{\"connector\":\"dockerhub.public\",\"entity\":\"${PARAM_IMAGE}\",\"namespace\":\"library\",\"icon_url\":\"\",\"attributes\":{\"additionalProp1\":[null],\"additionalProp2\":[null],\"additionalProp3\":[null]}}")
+imageDetails=$(curl -X POST "https://platform.slim.dev/orgs/rko.24nRz6GvLBo9hah9dqmhHON820R/collections/rkcol.2EADUkqrBkln6jbfc9RYbHiZVp7/images" -H  "accept: application/json" -H  "Authorization: Basic ${SAAS_KEY}" -H  "Content-Type: application/json" -d "{\"connector\":\"dockerhub.public\",\"entity\":\"${PARAM_IMAGE}\",\"namespace\":\"library\",\"icon_url\":\"\",\"attributes\":{\"additionalProp1\":[null],\"additionalProp2\":[null],\"additionalProp3\":[null]}}")
 imageId=$(jq -r '.data.id' <<< "${imageDetails}")
 nameSpace=$(jq -r '.data.namespace' <<< "${imageDetails}")
 entity=$(jq -r '.data.entity' <<< "${imageDetails}")
@@ -24,7 +24,7 @@ echo "${imageId}"
 
 
 
-curl -X POST "https://platform.slim.dev/orgs/rko.24nRz6GvLBo9hah9dqmhHON820R/collections/rkcol.24pqmb2bNiDQiBU2zAQMUJjWiSh/images/"${imageId}"/pins" -H  "accept: application/json" -H  "Authorization: Basic ${SAAS_KEY}" -H  "Content-Type: application/json" -d "{\"scope\":\"digest\",\"connector\":\"dockerhub.public\",\"entity\":\"${PARAM_IMAGE}\",\"namespace\":\"${nameSpace}\",\"version\":\"${tag}\",\"digest\":\"${shaId}\",\"os\":\"linux\",\"arch\":\"amd64\"}"
+curl -X POST "https://platform.slim.dev/orgs/rko.24nRz6GvLBo9hah9dqmhHON820R/collections/rkcol.2EADUkqrBkln6jbfc9RYbHiZVp7/images/"${imageId}"/pins" -H  "accept: application/json" -H  "Authorization: Basic ${SAAS_KEY}" -H  "Content-Type: application/json" -d "{\"scope\":\"digest\",\"connector\":\"dockerhub.public\",\"entity\":\"${PARAM_IMAGE}\",\"namespace\":\"${nameSpace}\",\"version\":\"${tag}\",\"digest\":\"${shaId}\",\"os\":\"linux\",\"arch\":\"amd64\"}"
 
 
 
