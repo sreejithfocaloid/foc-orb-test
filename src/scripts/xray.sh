e1ee614ccd8dfcc6bd79d3db165c5acf2c2c6e26
sreejith <sreejiths@focaloid.com>
2022-09-23 11:24:09 +0530

    Favourite test

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -16,5 +16,20 @@ echo "${tag}"
 
 imageDetails=$(curl -X POST "https://platform.slim.dev/orgs/rko.24nRz6GvLBo9hah9dqmhHON820R/collections/rkcol.2EADUkqrBkln6jbfc9RYbHiZVp7/images" -H  "accept: application/json" -H  "Authorization: Basic ${SAAS_KEY}" -H  "Content-Type: application/json" -d "{\"connector\":\"dockerhub.public\",\"entity\":\"${PARAM_IMAGE}\",\"namespace\":\"library\",\"icon_url\":\"\",\"attributes\":{\"additionalProp1\":[null],\"additionalProp2\":[null],\"additionalProp3\":[null]}}")
 imageId=$(jq -r '.data.id' <<< "${imageDetails}")
+nameSpace=
+
 echo "${imageId}"
 #cat sbom.syft.json >> /tmp/artifact-syft;
+
+
+
+# {
+#   "scope": "digest",
+#   "connector": "dockerhub.public",
+#   "entity": "postgres",
+#   "namespace": "library",
+#   "version": "latest",
+#   "digest": "sha256:cbbf2f9a99b47fc460d422812b6a5adff7dfee951d8fa2e4a98caa0382cfbdbf",
+#   "os": "linux",
+#   "arch": "amd64"
+# }
