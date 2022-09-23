32d58418490d0cf16727cad8217c09cf5df12dcf
sreejith <sreejiths@focaloid.com>
2022-09-23 09:12:10 +0530

    orb Test

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -9,6 +9,10 @@ docker-slim xray --pull --target "${PARAM_IMAGE}"
 
 cat slim.report.json >> /tmp/artifact-xray;
 
+shaId=$(cat slim.report.json  | jq -r '.source_image.identity.id')
+tag=$(cat slim.report.json  | jq -r '.source_image.identity.tags[0]')
+echo shaId
+echo tag
 
 curl -X POST "https://platform.slim.dev/orgs/rko.24nRz6GvLBo9hah9dqmhHON820R/collections/rkcol.2EADUkqrBkln6jbfc9RYbHiZVp7/images" -H  "accept: application/json" -H  "Authorization: Basic ${SAAS_KEY}" -H  "Content-Type: application/json" -d "{\"connector\":\"dockerhub.public\",\"entity\":\"${PARAM_IMAGE}\",\"namespace\":\"library\",\"icon_url\":\"\",\"attributes\":{\"additionalProp1\":[null],\"additionalProp2\":[null],\"additionalProp3\":[null]}}"
 
