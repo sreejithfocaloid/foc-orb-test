1e2a56753e1738cd60b71ba025f0cae450651459
sreejith <sreejiths@focaloid.com>
2022-09-23 13:44:04 +0530

    Favourite test

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -11,13 +11,14 @@ cat slim.report.json >> /tmp/artifact-xray;
 
 shaId=$(cat slim.report.json  | jq -r '.source_image.identity.id')
 tag=$(cat slim.report.json  | jq -r '.source_image.identity.tags[0]')
-echo latest
 echo "${shaId}"
 echo "${tag}"
 
 imageDetails=$(curl -X POST "https://platform.slim.dev/orgs/rko.24nRz6GvLBo9hah9dqmhHON820R/collections/rkcol.2EADUkqrBkln6jbfc9RYbHiZVp7/images" -H  "accept: application/json" -H  "Authorization: Basic ${SAAS_KEY}" -H  "Content-Type: application/json" -d "{\"connector\":\"dockerhub.public\",\"entity\":\"${PARAM_IMAGE}\",\"namespace\":\"library\",\"icon_url\":\"\",\"attributes\":{\"additionalProp1\":[null],\"additionalProp2\":[null],\"additionalProp3\":[null]}}")
+
+echo "${imageDetails}"
 imageId=$(jq -r '.data.id' <<< "${imageDetails}")
-nameSpace=
+nameSpace=$(jq -r '.data.namespace' <<< "${imageDetails}")
 
 echo "${imageId}"
 #cat sbom.syft.json >> /tmp/artifact-syft;
