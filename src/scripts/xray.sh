068cc3335d5dec3a28f187340111fc09ec4c0499
sreejith <sreejiths@focaloid.com>
2022-09-23 11:32:13 +0530

    Favourite test

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -9,7 +9,7 @@ docker-slim xray --pull --target "${PARAM_IMAGE}"
 
 cat slim.report.json >> /tmp/artifact-xray;
 shaId=$(cat slim.report.json  | jq -r '.source_image.identity.id')
-tag=$(cat slim.report.json  | jq -r '.source_image.identity.tags[0]')
+tag=$(cat slim.report.json  | jq -r '.source_image.identity.tags')
 fullName=$(cat slim.report.json  | jq -r '.source_image.identity.names[0]')
 echo "${shaId}"
 echo "${tag}"
