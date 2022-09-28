e131b2b9d39977218ac48639c39757d7dad57b4c
sreejith <sreejiths@focaloid.com>
2022-09-28 14:11:33 +0530

    SAAS Api Integration

--- src/scripts/vscan.sh
+++ src/scripts/vscan.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 
 
-echo "${CONNECTOR_ID}"
+echo Starting Vulnerability Scan : "${PARAM_IMAGE}"
 
-printenv
+jsonData="${XRAY_REQUEST}"
+command=vscan
+jsonDataUpdated=${jsonData//__CONNECTOR_ID__/${CONNECTOR_ID}}
+jsonDataUpdated=${jsonDataUpdated//__NAMESPACE__/${NAMESPACE}}
+jsonDataUpdated=${jsonDataUpdated//__REPO__/${PARAM_IMAGE}}
+jsonDataUpdated=${jsonDataUpdated//__COMMAND__/${command}}
+
+xrayRequest=$(curl -u ":${SAAS_KEY}" -X 'POST' \
+  "https://platform.slim.dev/orgs/${ORG_ID}/engine/executions" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d "${jsonDataUpdated}")
+
+executionId=$(jq -r '.id' <<< "${xrayRequest}")
+
+
+echo Starting Vulnerability Scan status check : "${PARAM_IMAGE}"
+
+
+
+executionStatus="unknown"
+while [[ ${executionStatus} != "completed" ]]; do
+	executionStatus=$(curl -s -u :"${SAAS_KEY}" https://platform.slim.dev/orgs/"${ORG_ID}"/engine/executions/"${executionId}" | jq -r '.state')
+    printf 'current NX state: %s '"$executionStatus \n"
+    [[ "${executionStatus}" == "failed" || "${executionStatus}" == "null" ]] && { echo "Vulnerability scan failed - exiting..."; exit 1; }
+    sleep 3
+done
+
+printf 'Vulnerability scan Completed state= %s '"$executionStatus \n"
+
+echo Fetching Vulnerability scan report : "${PARAM_IMAGE}"
+
+xrayReport=$(curl -L -u ":${SAAS_KEY}" -X 'GET' \
+  "https://platform.slim.dev/orgs/${ORG_ID}/engine/executions/${executionId}/result/report" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json')
+
+echo "${xrayReport}" >> /tmp/artifact-vscan;
+
+
+
+echo "${xrayReport}"
 
