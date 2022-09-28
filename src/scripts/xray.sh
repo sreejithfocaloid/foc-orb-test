195999bab6577b861c883b5a76485dbcd1921010
sreejith <sreejiths@focaloid.com>
2022-09-28 11:05:18 +0530

    SAAS Api Integration

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -43,7 +43,7 @@ echo Starting X-Ray Scan status check : "${PARAM_IMAGE}"
 
 executionStatus="unknown"
 while [[ ${executionStatus} != "completed" ]]; do
-	executionStatus=$(curl -s -u :"${SAAS_KEY}" https://platform.slim.dev/orgs/"${ORG_ID}"/engine/executions/fffff"${executionId}" | jq -r '.state')
+	executionStatus=$(curl -s -u :"${SAAS_KEY}" https://platform.slim.dev/orgs/"${ORG_ID}"/engine/executions/"${executionId}" | jq -r '.state')
     printf 'current NX state: %s '"$executionStatus \n"
     [[ "${executionStatus}" == "failed" || "${executionStatus}" == "null" ]] && { echo "XRAY failed - exiting..."; exit 1; }
     sleep 3
