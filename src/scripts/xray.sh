1474a247f1def4fc6d8980fd48f039825a3dcee2
sreejith <sreejiths@focaloid.com>
2022-09-28 11:41:46 +0530

    SAAS Api Integration

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -58,7 +58,7 @@ xrayReport=$(curl -u ":${SAAS_KEY}" -X 'GET' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json')
 
-link=$(echo "${xrayReport}"|grep -Eo 'http://[^ >]+'|head -1)
+link=$(echo "${xrayReport}"|grep -Eo 'https://[^ >]+'|head -1)
 
 echo "${link}"
 
