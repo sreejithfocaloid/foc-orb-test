a3a782b20a748f1e8cfa0c1f8049112fb456263a
sreejith <sreejiths@focaloid.com>
2022-09-28 15:21:37 +0530

    SAAS Api Integration

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -17,7 +17,6 @@ imageDetails=$(curl -u ":${SAAS_KEY}" -X "GET" \
 
 imageDetail=$(jq -r '.data[0]' <<< "${imageDetails}")
 
-echo "${imageDetail}"
 connectorId=$(jq -r '.connector' <<< "${imageDetail}")
 nameSpace=$(jq -r '.namespace' <<< "${imageDetail}")
 imageId=$(jq -r '.data.id' <<< "${imageDetail}")
@@ -80,6 +79,6 @@ echo "${fullName}"
 
 
 
-curl -u ":${SAAS_KEY}" -X POST "https://platform.slim.dev/orgs/${ORG_ID}/collections/${FAV_COLLECTION_ID}/images/"${imageId}"/pins" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"scope\":\"tag\",\"connector\":\"${connectorId}\",\"entity\":\"${entity}\",\"namespace\":\"${nameSpace}\",\"version\":\"${tag}\",\"digest\":\"\",\"os\":\"linux\",\"arch\":\"amd64\"}"
+curl -u ":${SAAS_KEY}" -X POST "https://platform.slim.dev/orgs/${ORG_ID}/collections/${FAV_COLLECTION_ID}/images/${imageId}/pins" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"scope\":\"tag\",\"connector\":\"${connectorId}\",\"entity\":\"${entity}\",\"namespace\":\"${nameSpace}\",\"version\":\"${tag}\",\"digest\":\"\",\"os\":\"linux\",\"arch\":\"amd64\"}"
 
 
